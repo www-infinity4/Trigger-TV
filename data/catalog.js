a4f26a4c7b7700664047c3f37db9d91d7e8fef27
www-infinity4 <ephraim.nwo@gmail.com>
2026-09-13 14:50:22 -0500

    Add western programming catalog

---
+++ data/catalog.js
@@ -0,0 +1,33 @@
+window.TRIGGER_CATALOG = [
+  {id:"bonanza-gunmen",title:"The Gunmen",series:"Bonanza",year:1959,rating:"TV-PG",kind:"episode",videoId:"fVi6afVdDCc",sourceStart:0,runtimeSeconds:2880,slotSeconds:3000},
+  {id:"bonanza-spanish-grant",title:"The Spanish Grant",series:"Bonanza",year:1959,rating:"TV-PG",kind:"episode",videoId:"fVi6afVdDCc",sourceStart:2905,runtimeSeconds:2860,slotSeconds:3000},
+  {id:"bonanza-desert-justice",title:"Desert Justice",series:"Bonanza",year:1960,rating:"TV-PG",kind:"episode",videoId:"fVi6afVdDCc",sourceStart:5797,runtimeSeconds:2860,slotSeconds:3000},
+  {id:"bonanza-feet-of-clay",title:"Feet of Clay",series:"Bonanza",year:1960,rating:"TV-PG",kind:"episode",videoId:"fVi6afVdDCc",sourceStart:8688,runtimeSeconds:2860,slotSeconds:3000},
+  {id:"bonanza-badge",title:"Badge Without Honor",series:"Bonanza",year:1960,rating:"TV-PG",kind:"episode",videoId:"fVi6afVdDCc",sourceStart:11549,runtimeSeconds:2860,slotSeconds:3000},
+  {id:"bonanza-mill",title:"The Mill",series:"Bonanza",year:1960,rating:"TV-PG",kind:"episode",videoId:"fVi6afVdDCc",sourceStart:14437,runtimeSeconds:2860,slotSeconds:3000},
+  {id:"bonanza-reckoning",title:"Day of Reckoning",series:"Bonanza",year:1960,rating:"TV-PG",kind:"episode",videoId:"fVi6afVdDCc",sourceStart:17324,runtimeSeconds:2860,slotSeconds:3000},
+  {id:"bonanza-viking",title:"The Last Viking",series:"Bonanza",year:1960,rating:"TV-PG",kind:"episode",videoId:"fVi6afVdDCc",sourceStart:20213,runtimeSeconds:2860,slotSeconds:3000},
+  {id:"bonanza-silent-thunder",title:"Silent Thunder",series:"Bonanza",year:1960,rating:"TV-PG",kind:"episode",videoId:"fVi6afVdDCc",sourceStart:23099,runtimeSeconds:2860,slotSeconds:3000},
+  {id:"bonanza-ape",title:"The Ape",series:"Bonanza",year:1960,rating:"TV-PG",kind:"episode",videoId:"fVi6afVdDCc",sourceStart:25984,runtimeSeconds:2860,slotSeconds:3000},
+
+  {id:"lr-telltale",title:"The Tell-Tale Bullet",series:"The Lone Ranger",year:1955,rating:"TV-PG",kind:"episode",videoId:"WUmv2fixyB8",sourceStart:0,runtimeSeconds:1440,slotSeconds:1800},
+  {id:"lr-false",title:"False Accusations",series:"The Lone Ranger",year:1955,rating:"TV-PG",kind:"episode",videoId:"WUmv2fixyB8",sourceStart:1445,runtimeSeconds:1440,slotSeconds:1800},
+  {id:"lr-arbuckle",title:"Adventure at Arbuckle",series:"The Lone Ranger",year:1955,rating:"TV-PG",kind:"episode",videoId:"WUmv2fixyB8",sourceStart:2889,runtimeSeconds:1440,slotSeconds:1800},
+  {id:"lr-harp",title:"A Harp for Hannah",series:"The Lone Ranger",year:1956,rating:"TV-PG",kind:"episode",videoId:"WUmv2fixyB8",sourceStart:4329,runtimeSeconds:1290,slotSeconds:1800},
+  {id:"lr-homer",title:"Homer with a High Hat",series:"The Lone Ranger",year:1955,rating:"TV-PG",kind:"episode",videoId:"WUmv2fixyB8",sourceStart:5620,runtimeSeconds:1530,slotSeconds:1800},
+
+  {id:"hoppy-devils-idol",title:"The Devil's Idol",series:"Hopalong Cassidy",year:1952,rating:"TV-PG",kind:"episode",videoId:"GQoBO9OU7Hs",sourceStart:0,runtimeSeconds:1530,slotSeconds:1800},
+  {id:"hoppy-sole-survivor",title:"The Sole Survivor",series:"Hopalong Cassidy",year:1952,rating:"TV-PG",kind:"episode",videoId:"GQoBO9OU7Hs",sourceStart:1546,runtimeSeconds:1530,slotSeconds:1800},
+  {id:"hoppy-valley-raiders",title:"The Valley Raiders",series:"Hopalong Cassidy",year:1952,rating:"TV-PG",kind:"episode",videoId:"GQoBO9OU7Hs",sourceStart:3090,runtimeSeconds:1530,slotSeconds:1800},
+  {id:"hoppy-twisted-trails",title:"Twisted Trails",series:"Hopalong Cassidy",year:1952,rating:"TV-PG",kind:"episode",videoId:"GQoBO9OU7Hs",sourceStart:4626,runtimeSeconds:1530,slotSeconds:1800},
+  {id:"hoppy-outlaws-reward",title:"The Outlaw's Reward",series:"Hopalong Cassidy",year:1952,rating:"TV-PG",kind:"episode",videoId:"6syIjstIFmM",sourceStart:0,runtimeSeconds:1530,slotSeconds:1800},
+  {id:"hoppy-grubstake",title:"Grubstake",series:"Hopalong Cassidy",year:1952,rating:"TV-PG",kind:"episode",videoId:"6syIjstIFmM",sourceStart:1540,runtimeSeconds:1530,slotSeconds:1800},
+  {id:"hoppy-steel-trails",title:"Steel Trails West",series:"Hopalong Cassidy",year:1952,rating:"TV-PG",kind:"episode",videoId:"6syIjstIFmM",sourceStart:3075,runtimeSeconds:1530,slotSeconds:1800},
+  {id:"hoppy-silent-testimony",title:"Silent Testimony",series:"Hopalong Cassidy",year:1952,rating:"TV-PG",kind:"episode",videoId:"6syIjstIFmM",sourceStart:4615,runtimeSeconds:1530,slotSeconds:1800},
+
+  {id:"movie-mclintock",title:"McLintock!",series:"Trigger Movie",year:1963,rating:"NR",kind:"movie",videoId:"dCMoVmR5LZw",sourceStart:0,runtimeSeconds:7620,slotSeconds:7800},
+  {id:"movie-blue-steel",title:"Blue Steel",series:"Trigger Movie",year:1934,rating:"NR",kind:"movie",videoId:"bJ-B7h-1yaM",sourceStart:0,runtimeSeconds:3240,slotSeconds:3600},
+  {id:"movie-angel-badman",title:"Angel and the Badman",series:"Trigger Movie",year:1947,rating:"NR",kind:"movie",videoId:"WET-_DMkL3k",sourceStart:0,runtimeSeconds:6000,slotSeconds:6300},
+  {id:"movie-man-utah",title:"The Man From Utah",series:"Trigger Movie",year:1934,rating:"NR",kind:"movie",videoId:"CWTbqUuDWys",sourceStart:0,runtimeSeconds:3300,slotSeconds:3600},
+  {id:"movie-one-eyed-jacks",title:"One-Eyed Jacks",series:"Trigger Movie",year:1961,rating:"NR",kind:"movie",videoId:"cWFYBfkmLI8",sourceStart:0,runtimeSeconds:8460,slotSeconds:8700}
+];
